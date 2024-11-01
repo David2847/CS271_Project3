e7fffef38c0ea9771c17c126a0c24f5af69ae441
David Jantz <dljantz@gmail.com>
2024-11-01 12:32:47 -0500

    Initial commit -- filled out project requirements

---
+++ Proj3_jantzd.asm
@@ -0,0 +1,32 @@
+TITLE Program Template     (template.asm)
+
+; Author: David Jantz
+; Last Modified: 11/1/2024
+; OSU email address: jantzd@oregonstate.edu
+; Course number/section:   CS271
+; Project Number: 3                Due Date: 11/3/2024
+; Description: This project collects temperature information from the
+;				user within a certain range and calculates some statistics
+;				based on that information.
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
